c2f47530f173948a50247206f09ce4607b28cef5
yangbinqi <ybq@outlook.com>
2017-07-20 23:06:56 +0800

    add forum

---
+++ src/Orchard.Web/Modules/NGM.Forum/Models/ForumPart.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NGM.Forum.Extensions;
+using NGM.Forum.Settings;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Aspects;
+using Orchard.ContentManagement.Records;
+using Orchard.Data.Conventions;
+
+namespace NGM.Forum.Models {
+    public class ForumPart : ContentPart<ForumPartRecord> {
+        public string Title {
+            get { return this.As<ITitleAspect>().Title; }
+        }
+
+        public string Description {
+            get { return Record.Description; }
+            set { Record.Description = value; }
+        }
+
+        public int ThreadCount {
+            get { return Record.ThreadCount; }
+            set { Record.ThreadCount = value; }
+        }
+
+        public int PostCount {
+            get { return Record.PostCount; }
+            set { Record.PostCount = value; }
+        }
+
+        public bool ThreadedPosts {
+            get { return Record.ThreadedPosts; }
+            set { Record.ThreadedPosts = value; }
+        }
+
+        public int Weight {
+            get { return Record.Weight; }
+            set { Record.Weight = value; }
+        }
+
+        public int ReplyCount {
+            get { return PostCount >= ThreadCount ? PostCount - ThreadCount : 0; }
+        }
+
+        public string PostType {
+            get {
+                var type = Settings.GetModel<ForumPartSettings>().PostType;
+                return !String.IsNullOrWhiteSpace(type) ? type : Constants.Parts.Post;
+            }
+        }
+
+        public string ThreadType {
+            get {
+                var type = Settings.GetModel<ForumPartSettings>().ThreadType;
+                return !String.IsNullOrWhiteSpace(type) ? type : Constants.Parts.Thread;
+            }
+        }
+    }
+
+    public class ForumPartRecord : ContentPartRecord {
+        [StringLengthMax]
+        public virtual string Description { get; set; }
+
+        public virtual int ThreadCount { get; set; }
+        public virtual int PostCount { get; set; }
+
+        public virtual bool ThreadedPosts { get; set; }
+
+        public virtual int Weight { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/NGM.Forum/Models/PostPart.cs
@@ -0,0 +1,41 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Aspects;
+using Orchard.ContentManagement.Records;
+using Orchard.Data.Conventions;
+
+namespace NGM.Forum.Models {
+    public class PostPart : ContentPart<PostPartRecord> {
+        public int? RepliedOn {
+            get { return Record.RepliedOn; }
+            set { Record.RepliedOn = value; }
+        }
+
+        public string Text {
+            get { return Record.Text; }
+            set { Record.Text = value; }
+        }
+
+        public string Format {
+            get { return Record.Format; }
+            set { Record.Format = value; }
+        }
+
+        public ThreadPart ThreadPart {
+            get { return this.As<ICommonPart>().Container.As<ThreadPart>(); }
+            set { this.As<ICommonPart>().Container = value; }
+        }
+
+        public bool IsParentThread() {
+            return RepliedOn == null;
+        }
+    }
+
+    public class PostPartRecord : ContentPartRecord {
+        public virtual int? RepliedOn { get; set; }
+
+        [StringLengthMax]
+        public virtual string Text { get; set; }
+
+        public virtual string Format { get; set; }
+    }
+}
